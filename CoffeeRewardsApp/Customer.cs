8ac28a835de01349e8920ba904d10bb40c9c5f32
Paul-kav <paulkavuma82@gmail.com>
2021-10-20 23:51:50 -0400

    Created Customer class with properties

---
+++ CoffeeRewardsApp/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace CoffeeRewardsApp
+{
+    public class Customer
+    {
+        public Customer()
+        {
+        }
+
+        public string customerId;
+        public string emailAddress;
+        public string firstName;
+        public string lastName; 
+
+        public int numberOfOrdersPlaced;
+        public double reward;
+        public int priceOfpurchasedItems;
+        public DateTime dateOfPurchase;
+
+        public void CalculateRewards()
+        {
+            double rewards = numberOfOrdersPlaced / reward;
+            Console.WriteLine($"{firstName} {lastName} got {reward} rewards.");
+        }
+    }
+}
---
+++ CoffeeRewardsApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CoffeeRewardsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("****** Welcome to my reards application ******");
+
+            Console.ReadLine();
+        }
+
+    }
+}
