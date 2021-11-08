807a5a5cc0c84536d7b2ffc44c34b3e239d00b40
Paul-kav <paulkavuma82@gmail.com>
2021-11-08 13:32:38 -0500

    Adding menu displays in the main program

--- CoffeeRewardsApp/Program.cs
+++ CoffeeRewardsApp/Program.cs
@@ -6,23 +6,29 @@ namespace CoffeeRewardsApp
     class Program
     {
 
-        //private static List<Customer> customer = new List<Customer>();
+        private static List<Customer> customer = new List<Customer>();
 
         static void Main(string[] args)
 
         {
             Console.ForegroundColor = ConsoleColor.Blue;
-            //Console.WriteLine("************");
-            Console.WriteLine("****** Welcome to my Coffee rewards application ******");
+            Console.WriteLine("********************************************");
+            Console.WriteLine("* Welcome To My Coffee Rewards Application *");
+            Console.WriteLine("********************************************");
+            Console.ForegroundColor = ConsoleColor.Green;
 
-            
+            string userSelection;
+
+
             //Customer paul = new Customer(004, "Paul", "Kavuma", "paul@gmail.com", 12);
             Gold peter = new Gold(005, "Peter", "Khan", "peterkhan@me.com", 2);
 
             //paul.DisplayCustomerDetails();
             peter.CalculateRewards();
             peter.DisplayCustomerDetails();
+
             
+
 
             Console.ReadLine();
         }
